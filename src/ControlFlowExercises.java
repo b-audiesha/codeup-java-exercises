fe76c7b0a133fc85d77700fa750a0e5adde51f27
audiesha brattin <a.brattin@gmail.com>
2021-03-04 19:57:05 -0600

    adding solutions

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -45,12 +45,12 @@ public class ControlFlowExercises {
     //Write a program that prints the numbers from 1 to 100.
 
     for (int x = 1; x <= 100; x++) {
-      if (x % 3 == 0) {
-        System.out.println("Fizz");
+      if (x % 15 == 0) {
+        System.out.println("FizzBuzz");
       } else if (x % 5 == 0) {
         System.out.println("Buzz");
-      } else if (x % 15 == 0) {
-        System.out.println("FizzBuzz");
+      }   if (x % 3 == 0) {
+        System.out.println("Fizz");
       } else System.out.println(x);
     }
   }
