b2a85f7226f03ab368d77b136598a45abda4a96d
audiesha brattin <a.brattin@gmail.com>
2021-03-10 09:34:08 -0600

    changing the name

--- src/PostStaticExample.java
+++ src/PostStaticExample.java
@@ -1,4 +1,4 @@
-public class Post {
+public class PostStaticExample {
 
   public static String nameOfBlog = "Justin's Blog";
 
@@ -10,7 +10,7 @@ public class Post {
     return String.format("%s has %d views.", title, numberOfPostViews);
   }
 
-  public static String getLongerBlogTitle(Post p1, Post p2) {
+  public static String getLongerBlogTitle(PostStaticExample p1, PostStaticExample p2) {
     if (p1.title.length() >= p2.title.length()) {
       return p1.title;
     } else {
@@ -19,13 +19,13 @@ public class Post {
   }
     public static void main(String[] args) {
         // create post 1
-        Post p1 = new Post();
+        PostStaticExample p1 = new PostStaticExample();
         p1.title = "A Day in the Life";
         p1.content = "Hello asd asdf asdf asdaf asdf.";
         p1.numberOfPostViews = 25;
 
         // create post 2
-        Post p2 = new Post();
+        PostStaticExample p2 = new PostStaticExample();
         p2.title = "My Dog, Snickers";
         p2.content = "Good doggy asfd asf asdf asdf asd fas asdf";
         p2.numberOfPostViews = 10;
@@ -35,10 +35,10 @@ public class Post {
         System.out.println(p2.returnViewsMessage());
 
         // using static method
-        System.out.println(Post.getLongerBlogTitle(p1, p2));
+        System.out.println(PostStaticExample.getLongerBlogTitle(p1, p2));
 
         // accessing static property
-        System.out.println(Post.nameOfBlog);
+        System.out.println(PostStaticExample.nameOfBlog);
     }
 
 }
