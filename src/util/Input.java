163175c9dc4f826307a515fa83f9e7348ef4f7c1
audiesha brattin <a.brattin@gmail.com>
2021-03-11 11:55:37 -0600

    adding solutions

---
+++ src/util/Input.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+  private Scanner scanner;
+
+  public Input() {
+    this.scanner = new Scanner(System.in);
+
+//    public static String getString() {
+//return scanner.nextLine()
+      //      String input = scanner.nextLine();
+    }
+//    boolean yesNo()
+//    int getInt(int min, int max)
+//    int getInt()
+//    double getDouble(double min, double max)
+//    double getDouble()
+
+
+
+  }
+}
+
+
+
+//Create a package inside of src named util. Inside of util, create a
+// class named Input that has a private property named scanner.
+// When an instance of this object is created, the scanner property
+// should be set to a new instance of the Scanner class.
+// The class should have the following methods, all of which return command line
+// input from the user:
