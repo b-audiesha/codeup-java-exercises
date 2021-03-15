2f424f1d1f762b26ecc1f5ef88abe55be3b07302
audiesha brattin <a.brattin@gmail.com>
2021-03-15 09:20:54 -0500

    adding dog.java notes

---
+++ src/JavaMethodDebug.java
@@ -0,0 +1,19 @@
+public class JavaMethodDebug {
+
+  // TODO: Fix the method below to work as expected.
+  //  The method should take in a string and character and return the number of instances of the passed character in the string.
+  public static int countLetters(String input, char character) {
+    int charCount = 0;
+    for (int i = 0; i < input.length(); i += 1) {
+      if (input.equals(String.valueOf(Character.toLowerCase(character)))) {
+        charCount++;
+      }
+    }
+    return charCount;
+  }
+
+  public static void main(String[] args) {
+    int numberOfSs = countLetters("MissiSSippi", 's');
+    System.out.println(numberOfSs); // should return 4
+  }
+}
