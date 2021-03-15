13e3e64e52d49c63ea1436c1b31097e7dc7695c2
audiesha brattin <a.brattin@gmail.com>
2021-03-15 09:25:48 -0500

    correcting Circle App

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,14 +1,15 @@
 package shapes;
 import util.Input;
 
-// Dont blame me for using cookie, I am currently super hungry.
 public class CircleApp {
+
   public static void main(String[] args) {
     Input input = new Input();
-    double radius = input.getDouble(0, 100000);
-    Circle cookie = new Circle(radius);
-    System.out.println(cookie.getCircumference());
-    System.out.println(cookie.getArea());
-
+    System.out.println("Please enter the radius of a circle");
+    double radius = input.getDouble();
+    Circle circle = new Circle(radius);
+    System.out.println(circle.getArea());
+    System.out.println(circle.getCircumference());
   }
+
 }
