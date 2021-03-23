c214a85404707c03e04b412fa8a57fc5c904d4ba
audiesha brattin <a.brattin@gmail.com>
2021-03-23 11:51:27 -0500

    addding

---
+++ src/ArraysPerson.java
@@ -0,0 +1,27 @@
+//
+//public class ArraysPerson {
+//
+//  private String name;
+//  private Job job;
+//
+//  public ArraysPerson() {
+//
+//  }
+//
+//  public String getName() {
+//    return name;
+//  }
+//
+//  public void setName(String name) {
+//    this.name = name;
+//  }
+//
+//  public Job getJob() {
+//    return job;
+//  }
+//
+//  public void setJob(Job job) {
+//    this.job = job;
+//  }
+//}
+
